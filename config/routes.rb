876170374a204ddd5f2c706ad28df017182a981a
Fredrick Chiyenda <fchiyenda@gmail.com>
2018-06-05 04:12:49 +0200

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :person_names
+  resources :person_attribute_types
+  resources :person_attributes
+  resources :person_addresses
+  resources :patient_identifier_types
+  resources :patient_identifiers
+  resources :global_properties
+  resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
